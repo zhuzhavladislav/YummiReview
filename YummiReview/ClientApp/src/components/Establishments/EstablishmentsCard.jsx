9be4bed8d999b2ed742e51b7c1dae2bfc408b0db
zhuzhavladislav <zhuzha_vladislav@mail.ru>
2023-04-05 02:41:01 +0400

    rebase and rdy to hosting

---
+++ YummiReview/ClientApp/src/components/Establishments/EstablishmentsCard.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import s from './Establishments.module.css'
+import city from "../../images/icons/city.svg"
+import address from "../../images/icons/address.svg"
+import money from "../../images/icons/money.svg"
+import phone from "../../images/icons/phone.svg"
+import kitchen from "../../images/icons/kitchen.svg"
+import Stars from '../Stars/Stars'
+
+
+const EstablishmentsCard = ({ data }) => {
+    let points = ((data.scoreFood + data.scoreService + data.scoreAtmosphere + data.scoreInterier) / 4).toFixed(1)
+    points = points > 5 ? 5 : points < 0 ? 0 : points
+
+    return (
+        <section className={s.item}>
+            <img alt={"Фото ресторана " + data.name} className={s.itemImage} src={`/api/establishment/images/${data.image}`} />
+            <div className={s.itemSecondColumn}>
+                <div className={s.itemTitle}>
+                    <h2 className={s.titleText}>
+                        {data.name}
+                    </h2>
+                    <div className={s.mark}>
+                        <p className={s.points}>{points}</p>
+                        <Stars points={points} />
+                        <p className={s.pointsText}>Оценка критика</p>
+                    </div>
+                </div>
+                <div className={s.itemInfo}>
+                    <p><img alt="Иконка кухни" className="item-icon" src={kitchen} /> {data.kitchen}</p>
+                    <p><img alt="Иконка города" className={s.itemIcon} src={city} /> {data.city}</p>
+                    <p><img alt="Иконка адреса" className={s.itemIcon} src={address} /> {data.street}</p>
+                    <p><img alt="Иконка денег" className={s.itemIcon} src={money} /> Средний чек {data.averageBill}₽</p>
+                    <p><img alt="Иконка телефона" className={s.itemIcon} src={phone} /> {data.phone}</p>
+                </div>
+                <Link className="button" style={{ marginTop: 10, marginBottom: 20 }} to={`/establishments/${data.id}`}>
+                    Подробнее
+                </Link>
+            </div>
+        </section>
+    )
+}
+
+export default EstablishmentsCard
---
+++ YummiReview/ClientApp/src/components/Main/MainCard.jsx
@@ -0,0 +1,23 @@
+import s from './Main.module.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Stars from '../Stars/Stars'
+
+const Card = ({ data }) => {
+    let points = ((data.scoreFood + data.scoreService + data.scoreAtmosphere + data.scoreInterier) / 4).toFixed(1)
+    points = points > 5 ? 5 : points < 0 ? 0 : points
+
+  return (
+    <section>
+      <Link to={`/establishments/${data.id}`}>
+        <img alt={"Помещение ресторана " + data.name} src={`/api/establishment/images/${data.image}`} />
+        <h3 className={s.categoryItemsTitle}>{data.name}</h3>
+        <Stars points={points} />
+        <p style={{marginTop: 10}} className={s.kitchen}>{data.kitchen}</p>
+        <p className={s.city}>{data.city}</p>
+      </Link>
+    </section>
+  )
+}
+
+export default Card
